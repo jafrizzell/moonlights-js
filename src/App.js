5222469e683998ca2516fda91a31511d145290fa
jafrizzell <j.a.frizzell@hotmail.com>
2022-10-16 21:29:27 -0500

    uhhhh

--- src/App.js
+++ src/App.js
@@ -150,7 +150,8 @@ class App extends React.Component {
           'mode': 'cors',
           'Allow-Control-Access-Origin': '*',
         },
-        body: JSON.stringify({date: d.toISOString().split('T')[0]})
+        body: JSON.stringify({date: d})
+        // body: JSON.stringify({date: d.toISOString().split('T')[0]})
       },
     )
       .then((res) => res.json())
